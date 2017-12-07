80b1eafb74748a38110e4b9ad11bc5e04ff8eef1
liuying <liuying@yryz.com>
2017-12-07 21:11:17 +0800

    all

---
+++ controlls/manager.js
@@ -0,0 +1,17 @@
+var HomeDAL = require("./../dal/home");
+var express = require('express');
+var Utils = require("./../utils");
+var Body = require("./../config/body");
+
+exports.getAll = function(req, res, next) {
+    HomeDAL.getAll().then(function(collections) {
+        var msg = {
+            navs: collections[0],
+            banner: collections[1]
+        };
+        res.render("manager", new Body(msg));
+    }).catch(function(e) {
+        req.flash("error", e.message);
+        res.redirect("manager");
+    });
+}
--- routes/manager.js
+++ routes/manager.js
@@ -7,69 +7,71 @@ var Footer = require("./../models/footer");
 var Nav = require("./../models/nav");
 var Body = require("./../config/body");
 var Article = require("./../models/article");
-var Home = {
-	render(req, res)  {
-		var findAll = [
-			this.get(Nav, 'navs', "导航获取失败"),
-			this.get(Banner, 'banners', "Banner获取失败"),
-			this.get(Intro, 'intro', "简介获取失败"),
-			this.get(TimeLine, 'timeline', "时光轴获取失败"),
-			this.get(Footer, 'footer', "页脚获取失败")
-		];
-		Promise.all(findAll).then( (result) => {
-			var body = new Body();
-			for(var item of result) {
-			    body.data[item.key] = item.collections;
-			}
-			body.data.type = 1;
-			res.render("manager/index", body);
-		}).catch((e) => {
-			req.flash("error", e.message);
-			res.redirect("/");
-		});
-	},
-	get(table, key, msg) {
-		return new Promise((resolve, reject) => {
-			table.find({}).sort({'serverTime': 1}).exec( (err, collections) => {
-				if (err) {
-					throw new Error(msg);
-				}
-				resolve({key,collections});
-			})
-		});
-	}
-}
+var ManagerControll = require("./../controlls/manager");
 
-router.get("/", function(req, res) {
-	Nav.find({}).sort({'serverTime': 1}).exec(function(err, collections) {
-		if (err) {
-			req.flash('error', "读取导航列表失败");
-			res.redirect("/manager");
-		} 
-		if (collections.length == 0) {
-			var messageBody = new Body({
-				navs: collections,
-				articles: [],
-				type: 0
-			});
-			return res.render("manager", messageBody);
-		}
-		var navId = collections[0]._id;
-		var categoryId = collections[0].categories[0]._id;
-		Article.find({navId, 'categoriesId.id': categoryId}).exec(function(error, coll) {
-			if (error) {
-				req.flash('error', "读取当前分类下的文章列表失败");
-				req.redirect("/manager");
-			} 
-			var messageBody = new Body({
-				navs: collections,
-				articles: coll,
-				type: 0
-			});
-			res.render("manager", messageBody);
-		});
-	});
-});
+// var Home = {
+// 	render(req, res)  {
+// 		var findAll = [
+// 			this.get(Nav, 'navs', "导航获取失败"),
+// 			this.get(Banner, 'banners', "Banner获取失败"),
+// 			this.get(Intro, 'intro', "简介获取失败"),
+// 			this.get(TimeLine, 'timeline', "时光轴获取失败"),
+// 			this.get(Footer, 'footer', "页脚获取失败")
+// 		];
+// 		Promise.all(findAll).then( (result) => {
+// 			var body = new Body();
+// 			for(var item of result) {
+// 			    body.data[item.key] = item.collections;
+// 			}
+// 			body.data.type = 1;
+// 			res.render("manager/index", body);
+// 		}).catch((e) => {
+// 			req.flash("error", e.message);
+// 			res.redirect("/");
+// 		});
+// 	},
+// 	get(table, key, msg) {
+// 		return new Promise((resolve, reject) => {
+// 			table.find({}).sort({'serverTime': 1}).exec( (err, collections) => {
+// 				if (err) {
+// 					throw new Error(msg);
+// 				}
+// 				resolve({key,collections});
+// 			})
+// 		});
+// 	}
+// }
+
+// router.get("/", function(req, res) {
+// 	Nav.find({}).sort({'serverTime': 1}).exec(function(err, collections) {
+// 		if (err) {
+// 			req.flash('error', "读取导航列表失败");
+// 			res.redirect("/manager");
+// 		} 
+// 		if (collections.length == 0) {
+// 			var messageBody = new Body({
+// 				navs: collections,
+// 				articles: [],
+// 				type: 0
+// 			});
+// 			return res.render("manager", messageBody);
+// 		}
+// 		var navId = collections[0]._id;
+// 		var categoryId = collections[0].categories[0]._id;
+// 		Article.find({navId, 'categoriesId.id': categoryId}).exec(function(error, coll) {
+// 			if (error) {
+// 				req.flash('error', "读取当前分类下的文章列表失败");
+// 				req.redirect("/manager");
+// 			} 
+// 			var messageBody = new Body({
+// 				navs: collections,
+// 				articles: coll,
+// 				type: 0
+// 			});
+// 			res.render("manager", messageBody);
+// 		});
+// 	});
+// });
 
 router.get("/navs/:navId/:categoryId", function(req, res) {
 	 var params = req.params;
@@ -98,6 +100,6 @@ router.get("/navs/:navId/:categoryId", function(req, res) {
 	});
 });
 
-router.get("/home", Home.render.bind(Home));
+router.get("/", ManagerControll.getAll);
 
 module.exports = router;
