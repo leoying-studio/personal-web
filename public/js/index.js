3e197e7f69e29f4ac0a93f8a5190089eeac2b15c
liuying <liuying@yryz.com>
2017-12-07 20:52:12 +0800

    all

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,13 @@ $(document).ready(function() {
          var category = JSON.parse(categories)[0]._id;
          window.location.href=window.location.origin+"/article/view/"+navId+"/"+category
      });
+     $("#paging").paging({
+        initPageNo: 1, // 初始页码
+        totalPages: 30, //总页数
+        totalCount: '合计300条数据', // 条目总数
+        slideSpeed: 600, // 缓动速度。单位毫秒 
+        callback: function(page) { // 回调函数 
+            console.log(page);
+        }
+     })
 });
