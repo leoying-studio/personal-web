bf173a963d3c0df19cbf9ca792f6a350442e403b
liuying <liuying@yryz.com>
2017-12-08 23:16:32 +0800

    all

--- public/js/index.js
+++ public/js/index.js
@@ -7,24 +7,26 @@ $(document).ready(function() {
      var currentPage =null;
      var total = 0
      var inited = false;
+
      if (params) {
          navId = params.navId;
          categoryId = params.categoryId;
          currentPage = params.currentPage;
          total = params.total;
      }
-    
 
-     // 返回到首页
+     // 返回到首页   
      $("#homePage").click(function() {
         window.location.href = "http://localhost:3000";
      });
+
      // 分类选择
      $('.category-menu').click(function() {
          var navId = $(this).attr('navid');
          var categoryId = $(this).attr('categoryId');
          window.location.href=window.location.origin+"/article/view/"+navId+"/"+categoryId+"/1"
      });
+     
      // 分页初始化
      $("#paging").paging({
         initPageNo: currentPage, // 初始页码
@@ -37,4 +39,6 @@ $(document).ready(function() {
             inited = true;
         }
      });
+
+
 });
