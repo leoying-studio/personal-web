0661acdc690ea43adc91b7311ed068ca2c9c7ef9
liuying <liuying@yryz.com>
2018-02-26 16:16:37 +0800

    a

--- models/special .js
+++ models/special .js
@@ -3,13 +3,8 @@ var Utils = require("./../utils/index");
 var DBSuper = require("./super");
 // 定义映射的集合结构模型
 var Scheam = new mongoose.Schema({
-	 background: String,
 	 title: String,  // 标题
-	 name: String,  // 专题名称
-	 themes: [
-		 {img: {type: String}, describe: {type: String}}
-	 ],
-	 discription: String,
+	 photo: String,
 	 serverTime: { type: String, default: Utils.getTime(new Date(), "s")}
 });
 
--- routes/home.js
+++ routes/home.js
@@ -5,24 +5,25 @@ var FooterModel = require("./../models/footer");
 var IntroModel = require("./../models/intro");
 var Validator = require("./../utils/validator");
 var ArticleModel = require("./../models/article");
+var Special = require("./../models/special");
 
-router.get("/",function(req, res) {
-	HomeProxy.getAll(function(data) {
-		res.render("index", data);
-	});
+router.get("/", function (req, res) {
+    HomeProxy.getAll(function (data) {
+        res.render("index", data);
+    });
 });
 
 /* GET home page. */
-router.get('/manager', function(req, res) {
-	HomeProxy.getAll(function(data) {
-		res.render("manager", data);
-	});
+router.get('/manager', function (req, res) {
+    HomeProxy.getAll(function (data) {
+        res.render("manager", data);
+    });
 });
 
 // 获取已推荐的数据
-router.get('/recommended/data', function(req, res) {
-    HomeProxy.getBanners(function(data) {
-        res.send( {
+router.get('/recommended/data', function (req, res) {
+    HomeProxy.getBanners(function (data) {
+        res.send({
             status: false,
             data
         });
@@ -30,58 +31,59 @@ router.get('/recommended/data', function(req, res) {
 });
 
 // 取消推荐
-router.post('/recommend/delete', function(req, res) {
-    ArticleModel.findOne({_id: req.body.articleId}, function(err, article) {
+router.post('/recommend/delete', function (req, res) {
+    ArticleModel.findOne({ _id: req.body.articleId }, function (err, article) {
         ArticleModel.update({
             _id: req.body.articleId
         }, {
-            $set: {recommend: false}
-        }, function(err, state) {
-            if (err) {
-                return res.send({
-                    status: false,
-                    message: "取消推荐失败"
+                $set: { recommend: false }
+            }, function (err, state) {
+                if (err) {
+                    return res.send({
+                        status: false,
+                        message: "取消推荐失败"
+                    });
+                }
+                res.send({
+                    status: true,
+                    message: "ok"
                 });
-            }
-            res.send({
-                status: true,
-                message: "ok"
             });
-        });
+    });
 });
 
-router.post("/footer/set", function(req, res) {
-	var body = req.body;
+router.post("/footer/set", function (req, res) {
+    var body = req.body;
     var background = req.body;
     var title = body.title;
     var description = body.title;
-	// 提交数据
-	new FooterModel({
-		background, title, descrption
-	}).save((err, footer) => {
-		if (err) {
-			req.flash("error", "页脚设置失败");
-		} else {
-			req.flash("success", "页脚设置成功");
-		}
-		res.redirect("/");
-	});
+    // 提交数据
+    new FooterModel({
+        background, title, descrption
+    }).save((err, footer) => {
+        if (err) {
+            req.flash("error", "页脚设置失败");
+        } else {
+            req.flash("success", "页脚设置成功");
+        }
+        res.redirect("/");
+    });
 });
 
-router.post("/intro/set", function() {
-	var query = query.query;
+router.post("/intro/set", function (req, res) {
+    var query = query.query;
     var title = query.title
     var caption = query.caption;
     var description = query.description;
     var backTitle = requeryq.backTitle;
     var backgrounds = query.backgrounds;
     try {
-        backgrounds = backgrounds.map(function(background) {
+        backgrounds = backgrounds.map(function (background) {
             return {
                 background
             }
         });
-    } catch(e) {
+    } catch (e) {
         backgrounds = [];
     }
     IntroModel.create({
@@ -90,27 +92,27 @@ router.post("/intro/set", function() {
         description,
         backTitle,
         backgrounds
-    }, function(err , doc) {
+    }, function (err, doc) {
         if (err) {
-			return reset.send({
-				message: "设置失败",
-				status: false
-			})
-        } 
+            return reset.send({
+                message: "设置失败",
+                status: false
+            })
+        }
         res.send({
-			message: "success",
-			status: true,
-			data: doc
-		});
+            message: "success",
+            status: true,
+            data: doc
+        });
     });
 });
 
-router.get("/intro/data", function(req, res) {
-    IntroModel.find({}, function(err, intros) {
+router.get("/intro/data", function (req, res) {
+    IntroModel.find({}, function (err, intros) {
         if (err) {
-           return res.send({
+            return res.send({
                 status: false
-           });
+            });
         }
         res.send({
             status: true,
@@ -118,4 +120,24 @@ router.get("/intro/data", function(req, res) {
         });
     });
 });
+
+router.post("/special/submit", function(req, res) {
+    var body = req.body;
+    var themeId = body.id;
+    var photo = body.photo;
+    var title = body.title;
+    Special.create({themeId, photo}, function(err, doc) {
+        if (err) {
+            return reset.send({
+                message: "添加失败",
+                status: false
+            })
+        } 
+        res.send({
+            status: true,
+            data: doc
+        });
+    });
+});
+
 module.exports = router;
