2fb3e2785156cebd583248620feaa996a37998e7
liuying <liuying@yryz.com>
2018-07-30 13:56:50 +0800

    提交

--- routes/categories.js
+++ routes/categories.js
@@ -7,11 +7,6 @@ router.post("/save", Validate.save, Categories.save, function(req, res, next) {
     next();
 });
 
-// 分类查询
-// router.get("/category/data", Validate.query, Categories.getChildren, function(req, res, next) {
-//      res.json(req.body.data);
-// });
-
 // 查询导航列表
 router.get("/data", Categories.getAll,  function(req, res, next) {
     res.json(req.body.data);
@@ -21,10 +16,5 @@ router.get("/data", Categories.getAll,  function(req, res, next) {
 router.post('/subCategory/save', Validate.subCategories, Categories.saveSubCategory, function(req, res, next) {
     next();
 });
-
-// 更新导航下面的类别
-// router.post("/children/update", Validate.children, function(req, res, next) {
-//     next();
-// });
 
 module.exports = router;
