151988421ff160925e92de2ca3a557da9cade6be
liuying <liuying@yryz.com>
2018-09-05 08:44:53 +0800

    u

--- routes/manager.js
+++ routes/manager.js
@@ -6,32 +6,7 @@ var Home = require('./../business/home');
 // 侧边栏切换进行重定向转发
 router.get('/redirect/:target', function(req, res, next) {
     var target = req.params.target;
-    switch(target) {
-        case '0' : {
-            res.render('manager/intro');
-            break;
-        } 
-        case '1': {
-            res.render('manager/theme');
-            break;  
-        }
-        case '2': {
-            res.render('manager/theme.detail.jade');
-            break;
-        }
-        case '4' :{
-            res.render('manager/categoies');
-            break;
-        }
-        case '5' : {
-            res.render('manager/subcategories');
-        }
-        case '6' : {
-            res.render('manager/articles');
-        }
-    }   
+    res.render("manager/" + target);  
 });
-
-
 
 module.exports = router;
