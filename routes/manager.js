422f197ab5964ddb20a6bcf23f9293ec14b9137b
liuying <liuying@yryz.com>
2018-09-07 13:23:27 +0800

    解决分类模板不显示的问题

--- routes/manager.js
+++ routes/manager.js
@@ -6,30 +6,7 @@ var Home = require('./../business/home');
 // 侧边栏切换进行重定向转发
 router.get('/redirect/:target', function(req, res, next) {
     var target = req.params.target;
-    switch(target) {
-        case '0' : {
-            res.render('manager/intro');
-            break;
-        } 
-        case '1': {
-            res.render('manager/theme');
-            break;  
-        }
-        case '2': {
-            res.render('manager/theme.detail');
-            break;
-        }
-        case '4' :{
-            res.render('manager/categoies.jade');
-            break;
-        }
-        case '5' : {
-            res.render('manager/subcategories.jade');
-        }
-        case '6' : {
-            res.render('manager/articles');
-        }
-    }   
+    res.render("manager/" + target);  
 });
 
 module.exports = router;
