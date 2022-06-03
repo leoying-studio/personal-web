3360350b7a6364efcbf1c14a50d2f0b9671062da
liuying <934725772@qq.com>
2022-06-03 11:02:44 +0800

    初始化管理后台界面

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const lessMiddleware = require('less-middleware');
+const rootPath = __dirname
+const publicPath = path.join(rootPath, "public") ;
+
 const db = require('./db')
 import routes from './routes'
 
-const staticPath = path.join(__dirname, "/public")
 
-const lessMiddleware = require('less-middleware');
-app.use(lessMiddleware( path.join(__dirname, "public", "styles")));
+app.use(lessMiddleware('/public/styles', {
+    dest: '/public',
+    force: true,
+    debug: true,
+    pathRoot: rootPath,
+    preprocess: {
+        path: function(pathname, req) {
+            return pathname.replace(path.sep + 'styles' + path.sep, path.sep)
+        }
+      }
+}));
+
+app.use(express.static(publicPath));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes)
-app.use(express.static(staticPath));
+
+
+
 module.exports = app;
--- interceptor/check.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    login: function (req, res, next) {
-        // if (!req.session.user &&　req.url === '/manager') {
-        //     req.flash('error', '未登录');
-        //     return res.redirect('/user/login/view');
-        // }
-        next();
-    }
-}
