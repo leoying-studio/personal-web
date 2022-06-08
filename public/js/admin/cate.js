8ec3350700d35002557d724b96668b09eff9d545
yryz <yryz@yryzdeMacBook-Pro-2.local>
2022-06-08 18:00:56 +0800

    fix: merge

--- public/js/admin/cate.js
+++ public/js/admin/cate.js
@@ -4,6 +4,11 @@ $.get("/categories/tree", function(res) {
         data.forEach(function(item) {
             item.text = item.label;
             item.nodes = item.children;
+            item.state = {
+                expanded: false
+            }
+            item.icon = "icon iconfont icon-add-bold"
+            item.selectedIcon = "icon iconfont icon-minus-bold"
             if (item.children.length) {
                 formateTree(item.nodes)
             }
@@ -11,16 +16,37 @@ $.get("/categories/tree", function(res) {
     }
 
     formateTree(res)
-    $("#categoriesTree").treeview({
-        data: res
-    })
 
+    $("#categoriesTree").treeview({
+        data: res,
+        showCheckbox:false,
+        multiSelect:false
+    })
 
     $('#categoriesTree').on('nodeChecked', function(event, data) {
         // Your logic goes here
     });
 })
 
+
+var modalInstance = null
+
+$("#confirmationOkBtn").click(function() {
+    // $("#confirmationBox").modal('hide')
+})
+
+function onOperationCate(value, item, index) {
+    return "<button class='btn btn-info mr-3' id='editBtn'>编辑</button><button class='btn btn-danger' id='removeBtn'>删除</button>"
+}
+
+var operationEvents = {
+   'click #editBtn': function (e, value, row, index) {
+       
+   },
+   'click #removeBtn': function (e, value, row, index) {
+    // modalInstance = $("#confirmationBox").modal('show')
+   }
+}
 
 $("#saveCategoryBtn").click(function() {
     var label = $("#categoryName").val();
